61fb33ddc61671b0187ea9d993c16808c97bb448
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2018-05-06 12:55:57 -0300

    chapter 54

--- src/app/course-detail/course-detail.component.ts
+++ src/app/course-detail/course-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-course-detail',
@@ -9,13 +10,23 @@ import { ActivatedRoute } from '@angular/router';
 export class CourseDetailComponent implements OnInit {
 
   id: string;
+  inscription: Subscription
 
   constructor(private route: ActivatedRoute) { 
-    console.log(this.route);
-    this.id = this.route.snapshot.params['id'];
+    //console.log(this.route);
+    //this.id = this.route.snapshot.params['id'];
   }
 
   ngOnInit() {
+    this.inscription = this.route.params.subscribe(
+      (params:any) => {
+        this.id = params ['id'];
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.inscription.unsubscribe();
   }
 
 }
